926785c5de09a8dc6a14f58588eb39f66b327cc0
wiistreamcom <wiistreamcom@gmail.com>
2013-09-10 09:37:53 +0000

    Future

---
+++ sql/chars_keyitems.sql
@@ -0,0 +1,24 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: database
+Target Host: localhost
+Target Database: database
+Date: 9/10/2013 12:55:34 AM
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for chars_keyitems
+-- ----------------------------
+CREATE TABLE `chars_keyitems` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `charid` int(255) NOT NULL DEFAULT '0',
+  `keyid` int(255) NOT NULL DEFAULT '0',
+  `keyname` varchar(60) NOT NULL DEFAULT 'noname',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
