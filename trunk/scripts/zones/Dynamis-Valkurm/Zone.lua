c7a040b80dbb51ac2f704c1aec3e68211ac81c55
wiistreamcom@gmail.com <wiistreamcom@gmail.com@01a2f1cc-d7d1-39b7-8589-f76901b78278>
2013-09-13 02:20:19 +0000

    currently the airships or any lua zoning system is not working. im setting the POS for the zone system in the database and i needed to cancel out the dynamis lus for a while.. wallhack command updated

--- trunk/scripts/zones/Dynamis-Valkurm/Zone.lua
+++ trunk/scripts/zones/Dynamis-Valkurm/Zone.lua
@@ -22,35 +22,7 @@ end;
 function onZoneIn(player,prevZone)
 	cs = -1;
 	
-	local realDay = os.time();
-	local dynaWaitxDay = player:getVar("dynaWaitxDay");
 	
-	if((dynaWaitxDay + (BETWEEN_2DYNA_WAIT_TIME * 24 * 60 * 60)) < realDay or player:getVar("DynamisID") == GetServerVariable("[DynaValkurm]UniqueID")) then
-		if(player:isBcnmsFull() == 1) then
-			if(player:hasStatusEffect(EFFECT_DYNAMIS, 0) == false) then
-				inst = player:addPlayerToDynamis(1286);
-				
-				if(inst == 1)then
-					player:bcnmEnter(1286);
-				else
-					 cs = 0x0065;
-				end
-			else
-				player:bcnmEnter(1286);
-			end
-		else
-			inst = player:bcnmRegister(1286);
-			
-			if(inst == 1)then
-				player:bcnmEnter(1286);
-			else
-				cs = 0x0065;
-			end
-		end
-	else
-		cs = 0x0065;
-	end
-
 	return cs;
 end;
 
@@ -78,9 +50,6 @@ end;
 function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
-	if(csid == 0x0065) then
-		player:setPos(117,-9,132,162,103);
-	end
 end;
 
 
