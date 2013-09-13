a037e4d48a4e4508f80eae4faa87887a740cb9bf
wiistreamcom <wiistreamcom@gmail.com>
2013-09-13 02:20:19 +0000

    currently the airships or any lua zoning system is not working. im setting the POS for the zone system in the database and i needed to cancel out the dynamis lus for a while.. wallhack command updated

--- scripts/zones/Dynamis-Windurst/Zone.lua
+++ scripts/zones/Dynamis-Windurst/Zone.lua
@@ -24,35 +24,7 @@ function onZoneIn(player,prevZone)
 	
 	cs = -1;
 	
-	local realDay = os.time();
-	local dynaWaitxDay = player:getVar("dynaWaitxDay");
 	
-	if((dynaWaitxDay + (BETWEEN_2DYNA_WAIT_TIME * 24 * 60 * 60)) < realDay or player:getVar("DynamisID") == GetServerVariable("[DynaWindurst]UniqueID")) then
-		if(player:isBcnmsFull() == 1) then
-			if(player:hasStatusEffect(EFFECT_DYNAMIS, 0) == false) then
-				inst = player:addPlayerToDynamis(1282);
-				
-				if(inst == 1)then
-					player:bcnmEnter(1282);
-				else
-					 cs = 0;
-				end
-			else
-				player:bcnmEnter(1282);
-			end
-		else
-			inst = player:bcnmRegister(1282);
-			
-			if(inst == 1)then
-				player:bcnmEnter(1282);
-			else
-				cs = 0;
-			end
-		end
-	else
-		cs = 0;
-	end
-
 	return cs;
 	
 end;
@@ -81,8 +53,5 @@ function onEventFinish(player,csid,option)
 --printf("CSID: %u",csid);
 --printf("RESULT: %u",option);
 	
-	if(csid == 0) then
-		player:setPos(-217.000,1.000,-119.000,94,0xEF);
-	end
 	
 end;
